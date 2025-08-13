ffafc1064b72c6ad811b510d3c09aeff3a51688c
IAmBatJew <aamigarr@gmail.com>
2025-08-12 22:21:49 -0400

    adding top-bottom padding and footer

--- src/App.js
+++ src/App.js
@@ -36,6 +36,55 @@ function App() {
         </div>
       </nav>
       <LandingPage />
+      <footer className="footer">
+        <p className="copyright">&copy; 2023 Anderson Orchard. All rights reserved.</p>
+        <div className="footer_category">
+          <h4>Featured Products</h4>
+          <ul>
+            <li>Apples</li>
+            <li>Oranges</li>
+            <li>Pears</li>
+          </ul>
+        </div>
+        <div className="footer_category">
+          <h4>Attractions</h4>
+          <ul>
+            <li>Hayrides</li>
+            <li>Apple Picking</li>
+            <li>Apple Barn</li>
+            <li>Field Trips</li>
+          </ul>
+        </div>
+        <div className="footer_category">
+          <h4>Forms</h4>
+          <ul>
+            <li>Join Our Mailing List</li>
+            <li>Book a Field Trip</li>
+          </ul>
+        </div>
+        <div className="footer_category">
+          <h4>General Information</h4>
+          <div className="double-column">
+            <ul>
+              <li>Hours of Operation</li>
+              <li>Orchard Map</li>
+              <li>Mailing List</li>
+            </ul>
+            <ul>
+              <li>News</li>
+              <li>Directions</li>
+              <li>Contact Us</li>
+              <li>Our History</li>
+            </ul>
+          </div>
+        </div>
+        <div className="company_info">
+          <h4>Anderson Orchard</h4>
+          <p>369 East Greencastle Road<br />
+            Mooresville, IN 46158<br />
+            317-831-4181</p>
+        </div>
+      </footer>
     </div>
   );
 }
